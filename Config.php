54c0a95507213d7a429c6745c746fd5d05b76961
ali <ali.ashkaran@gmail.com>
2020-03-30 21:46:41 +0430

    Initial commit

--- Config.php
+++ Config.php
@@ -8,7 +8,7 @@ class Config
 
     const APPNAME       = "MzQ";
     const DEBUG_MODE    = true;
-    const WEBSITE_URL       = "http://195.248.243.220/MzQ/";
+    const WEBSITE_URL       = "";
 
 
     const servername    = "localhost";//میزبان
